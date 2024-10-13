3c552d34820b5b2d865bf285ac2aaffbce2acb69
Matheus Souza <matheusouza1997@gmail.com>
2024-10-13 15:22:18 -0300

    Inicio do projeto

---
+++ app/Models/Project.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'ends_at',
+        'status',
+        'tech_stack',
+        'created_by',
+    ];
+
+    public function casts()
+    {
+        return [
+            'tech_stack' => 'array',
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(200)->create();
+
+        User::query()->inRandomOrder()->limit(10)->get()->each(fn(User $user) => Project::create(['created_by' => $user->id]));
+
+        Project::factory(10)->create();
+    }
+}
